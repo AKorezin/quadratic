c24b01c05f56064bfc206ce2b192f3c015013e0f
AKorezin <source-email@yandex.ru>
2014-12-14 02:12:10 +0300

    bugfixes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,7 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
 	QStringList k=ui->lineEdit->text().split(QRegExp("\\s"),QString::SkipEmptyParts);
-	if(ui->lineEdit->text().contains(QRegExp("[^1-9\\si+-]")) or chck(k))
+	if(ui->lineEdit->text().contains(QRegExp("[^0-9\\si+-]")) or chck(k))
 	{
 		ui->statusBar->showMessage("Ошибка записи");
 		return;
@@ -32,8 +32,19 @@ void MainWindow::on_pushButton_clicked()
 	QList<qreal> k_sep;
 	k_sep=parseList(k);
 	solve(k_sep);
-	ui->label->setText("Первый корень: "+QString::number(x1[0])+"+"+QString::number(x1[1])+"i");
-	ui->label_2->setText("Второй корень: "+QString::number(x2[0])+"+"+QString::number(x2[1])+"i");
+	QString first,second;
+	if(x1[1]==0 and x2[1]==0)
+	{
+		first=QString("Первый корень: %1").arg(x1[0],0,'g',5);
+		second=QString("Второй корень: %1").arg(x2[0],0,'g',5);
+	}
+	else
+	{
+		first=QString("Первый корень: %1+%2i").arg(x1[0],0,'g',5).arg(x1[1],0,'g',5);
+		second=QString("Второй корень: %1+%2i").arg(x2[0],0,'g',5).arg(x2[1],0,'g',5);
+	}
+	ui->label->setText(first);
+	ui->label_2->setText(second);
 }
 
 QList<qreal> MainWindow::parseList(QStringList klist)
@@ -76,28 +87,42 @@ void MainWindow::solve(QList<qreal> k)
 	qreal a,b;
 	a=k[2]*k[2]-k[3]*k[3]-4*k[0]*k[4]+4*k[1]*k[5];
 	b=2*k[2]*k[3]-4*k[0]*k[5]-4*k[1]*k[4];
-	qDebug()<<a<<b;
-	qreal c1,c2,l,atan;
-	l=qPow(a*a+b*b,0.25);
-	atan=qAtan(b/a);
-	if (a<0)
-		atan+=M_PI;
-	c1=qCos(atan/2)*l;
-	c2=qSin(atan/2)*l;
-	qDebug()<<c1<<c2;
 	qreal u1,u2,d1,d2;
-	d1=k[0]*2;
-	d2=k[1]*2;
+	if(a==0 and b==0)
+	{
+		d1=k[0]*2;
+		d2=k[1]*2;
 
-	u1=-k[2]+c1;
-	u2=-k[3]+c2;
-	x1[0]=(u1*d1+u2*d2)/(d1*d1+d2*d2);
-	x1[1]=(d1*u2-d2*u1)/(d1*d1+d2*d2);
+		u1=-k[2];
+		u2=-k[3];
+		x1[0]=(u1*d1+u2*d2)/(d1*d1+d2*d2);
+		x1[1]=(d1*u2-d2*u1)/(d1*d1+d2*d2);
+		x2[0]=x1[0];
+		x2[1]=x1[1];
+	}
+	else
+	{
+		qreal c1,c2,l,atan;
+		l=qPow(a*a+b*b,0.25);
+		atan=qAtan(b/a);
+		if (a<0)
+			atan+=M_PI;
+		c1=qCos(atan/2)*l;
+		c2=qSin(atan/2)*l;
 
-	u1=-k[2]-c1;
-	u2=-k[3]-c2;
-	x2[0]=(u1*d1+u2*d2)/(d1*d1+d2*d2);
-	x2[1]=(d1*u2-d2*u1)/(d1*d1+d2*d2);
+		d1=k[0]*2;
+		d2=k[1]*2;
+
+		u1=-k[2]+c1;
+		u2=-k[3]+c2;
+		x1[0]=(u1*d1+u2*d2)/(d1*d1+d2*d2);
+		x1[1]=(d1*u2-d2*u1)/(d1*d1+d2*d2);
+
+		u1=-k[2]-c1;
+		u2=-k[3]-c2;
+		x2[0]=(u1*d1+u2*d2)/(d1*d1+d2*d2);
+		x2[1]=(d1*u2-d2*u1)/(d1*d1+d2*d2);
+	}
 }
 
 bool MainWindow::chck(QStringList list)
