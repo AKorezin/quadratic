d95d3ec907ec9886f1ad3da0aadb39971fbec8e0
AKorezin <source-email@yandex.ru>
2014-12-13 20:55:41 +0300

    skeleton

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
@@ -11,4 +12,35 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+	QStringList k=ui->lineEdit->text().split(QRegExp("\\s"));
+	if (k.size()!=3)
+	{
+		ui->statusBar->showMessage("Уравнение не квадратное");
+		return;
+	}
+	else
+	{
+		ui->statusBar->clearMessage();
+	}
+	QList<qreal> k_sep;
+	k_sep=parseList(k);
+	solve(k_sep);
+	ui->label->setText("Первый корень: "+QString::number(x1));
+	ui->label_2->setText("Второй корень: "+QString::number(x2));
+}
+
+QList<qreal> MainWindow::parseList(QStringList klist)
+{
+	QList<qreal> k_sep;
+	return k_sep;
+}
+
+void MainWindow::solve(QList<qreal> k_sep)
+{
+	x1=0;
+	x2=0;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,14 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+private slots:
+	void on_pushButton_clicked();
+
 private:
 	Ui::MainWindow *ui;
+	qreal x1,x2;
+	QList<qreal> parseList(QStringList);
+	void solve(QList<qreal>);
 };
 
 #endif // MAINWINDOW_H
